1e6497cc9ee22c70044dfc719f42a375a05f74c8
Tanya Tomchuk <tanyem@protonmail.com>
2018-01-24 15:41:04 +0300

    Added podfile and delegat for work with Firabase

---
+++ Pods/Target Support Files/Pods-GOSU/Pods-GOSU-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GOSU : NSObject
+@end
+@implementation PodsDummy_Pods_GOSU
+@end
---
+++ Pods/Target Support Files/Pods-GOSUTests/Pods-GOSUTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GOSUTests : NSObject
+@end
+@implementation PodsDummy_Pods_GOSUTests
+@end
