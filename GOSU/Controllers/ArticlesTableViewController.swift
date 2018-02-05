406082b385060b4f5d246bddea3e5bb1edf5e254
Tanya Tomchuk <tanyem@protonmail.com>
2018-02-05 18:31:14 +0300

    Received all the articles from Firebase

--- GOSU/Controllers/ArticlesTableViewController.swift
+++ GOSU/Controllers/ArticlesTableViewController.swift
@@ -7,85 +7,90 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
 class ArticlesTableViewController: UITableViewController {
 
     var countryID = 0
-
+    var refArticles: DatabaseReference?
+    var articlesList = [ArticlesData]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print("wow\(countryID)")
+        refArticles = Database.database().reference().child("articles")
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+        //observing the data changes
+        refArticles?.observe(DataEventType.value, with: { (snapshot) -> Void in
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
+            print(snapshot)
+            if snapshot.childrenCount > 0 {
+                //clearing the list
+                self.articlesList.removeAll()
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+                //iterating through all the values
+                for articles in snapshot.children.allObjects as! [DataSnapshot] {
+                    //getting values
+                    let articleObject = articles.value as? [String: AnyObject]
+                    let articleTitle  = articleObject?["title"]
+                    let articleDescription = articleObject?["description"]
+                    let articleImage = articleObject?["image"]
+                    let categoryID = articleObject?["id"]
+                    let articleBody = articleObject?["body"]
+                    let articleVideo = articleObject?["video_link"]
+
+                    //creating artist object with model and fetched values
+                    let category = ArticlesData(title: articleTitle as! String?,
+                                                description: articleDescription as! String?,
+                                                pictureURL: articleImage as! String?,
+                                                category_id: categoryID as! Int?,
+                                                body: articleBody as! String?,
+                                                video_link: articleVideo as! String?)
+
+                    //appending it to list
+                    self.articlesList.append(category)
+                }
+
+                //reloading the tableview
+                self.tableView.reloadData()
+
+            }
+        })
+        
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return articlesList.count
     }
 
-    /*
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleCell", for: indexPath) as! ArticlesTableViewCell
 
         // Configure the cell...
+        //the artist object
+        let article: ArticlesData
+
+        //getting the artist of selected position
+        article = articlesList[indexPath.row]
+
+        //adding values to labels
+        cell.articleTitle.text = article.title
+        cell.articleDescription.text = article.description
+        cell.articleImage.image = try! UIImage(data: Data(contentsOf: URL(string: article.pictureURL!)!))
+        cell.articleImage.layer.cornerRadius = 90 / 2
+        cell.articleImage.layer.borderWidth = 1.0
+        cell.articleImage.layer.borderColor = UIColor.white.cgColor
+        cell.articleImage.clipsToBounds = true
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
+    
 
     /*
     // MARK: - Navigation
