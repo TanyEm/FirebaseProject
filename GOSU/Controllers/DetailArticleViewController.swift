132e9a46d37d6ab45079002a314792963f7b1b4b
Tanya Tomchuk <tanyem@protonmail.com>
2018-02-07 19:05:35 +0300

    Added new screen for Articles

---
+++ GOSU/Controllers/DetailArticleViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DetailArticleViewController.swift
+//  GOSU
+//
+//  Created by Tanya Tomchuk on 07/02/2018.
+//  Copyright © 2018 Tanya Tomchuk. All rights reserved.
+//
+
+import UIKit
+
+class DetailArticleViewController: UIViewController {
+    @IBOutlet weak var articleImg: UIImageView!
+    @IBOutlet weak var articleTitle: UILabel!
+    @IBOutlet weak var articleBody: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
